7f955573cd926d01a2fc584efe09c4a39942a69c
WRITERDRO <devasking09@gmail.com>
2025-07-06 07:24:58 +0530

    Add files via upload

--- VIP_INNOCENT/__init__.py
+++ VIP_INNOCENT/__init__.py
@@ -1,27 +1,7 @@
-from VIP_INNOCENT.core.bot import INNOCENT
-from VIP_INNOCENT.core.dir import dirr
-from VIP_INNOCENT.core.git import git
-from VIP_INNOCENT.core.userbot import Userbot
-from VIP_INNOCENT.misc import dbb, heroku
-from pyrogram import Client
-from SafoneAPI import SafoneAPI
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = INNOCENT()
-api = SafoneAPI()
-userbot = Userbot()
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .extraction import *
+from .formatters import *
+from .inline import *
+from .sys import *
